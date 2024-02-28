4802e98a31ab101f0d17bf738f3e86687146bae2
Colin McBride <c.mcbride@eagles.oc.edu>
2024-02-28 15:49:01 -0600

    Fixed config detect. Program wouldn't work if img2 had more points than img1

--- Development/Mesh Generation/cameraLaserConfigDetect.py
+++ Development/Mesh Generation/cameraLaserConfigDetect.py
@@ -33,12 +33,22 @@ from imageTools import *
 CONSTANTS
 '''
 UNITS = "in"    # Distance units used 
-DIST_1 = 6.1189      # Image 1 distance 
-DIST_2 = 8.0089      # Image 2 distance
 DISTANCES = [     # Array with distance values. The length of this array determines how many pictures will be taken. 
    6.1189,
    8.0089
 ]
+'''
+distances = [
+      4.2289,
+      5.3629,
+      6.4969,
+      7.6309,
+      8.7649,
+      9.8989,
+      11.0329,
+      12.1669
+   ]
+'''
 HFOV = 55          # Horizontal FOV of the camera
 
 '''
@@ -96,12 +106,6 @@ def estimateConfiguration(
    dLaserPoints = []
    thetaLaserPoints = []
 
-   # Make sure img1points has the fewest number of points
-   if len(img2points) > len(img1points):
-      temp = img1points
-      img1points = img2points
-      img2points = temp
-
    # Loop through all points in the images
    for (x1, y1) in img1points:
       # Extract points
@@ -136,9 +140,11 @@ def estimateConfiguration(
 MAIN
 '''
 def main():
-   '''
+   
    # Initialization
-   picsRemaining = 2
+   numImages = len(DISTANCES)
+   picsRemaining = len(DISTANCES)
+   images = []
 
    # Set up video capture 
    cap = setupVideoCapture(verbose=True)
@@ -157,66 +163,36 @@ def main():
       key = cv2.waitKey(1)
       if (key & 0xFF) == ord(' '):
          # Save current frame
-         if picsRemaining == 2:
-            image1 = frame.copy()
-            cv2.imwrite("configDetectImg1.png", image1)
-            print("Image 1 captured!")
-         elif picsRemaining == 1:
-            image2 = frame.copy()
-            cv2.imwrite("configDetectImg2.png", image2)
-            print("Image 2 captured!")
-         picsRemaining -= 1
+         if picsRemaining > 0:
+            images.append(frame.copy())
+            picsRemaining -= 1
+            print(f"Image {numImages - picsRemaining} of {numImages} captured!")
       elif (key & 0xFF) == ord('q'):
          # Quit
          break
    # end while picsRemaining > 0
    
-   # Process images (if both were captured)
+   # Process images (if all were captured)
    if picsRemaining == 0:
-      config = estimateConfiguration(image1, DIST_1, image2, DIST_2, np.deg2rad(HFOV))
-      print(f"dLaser: {config.dLaser} {UNITS}")
-      print(f"thetaLaser: {np.rad2deg(config.thetaLaser)} deg")
-      print(f"phiLaser: {np.rad2deg(config.phiLaser)} deg")
-      print(f"Total configuration: {config}")
+      # Perform all possible permutations
+      dLaserPoints = []
+      thetaLaserPoints = []
+      phiLaserPoints = []
+      for i in range(numImages):
+         for j in range(i + 1, numImages):
+            config = estimateConfiguration(images[i], DISTANCES[i], images[j], DISTANCES[j], np.deg2rad(HFOV))
+            dLaserPoints.append(config.dLaser)
+            thetaLaserPoints.append(config.thetaLaser)
+            phiLaserPoints.append(config.phiLaser)
+      
+      # Average results from all 
+      dLaser = np.average(dLaserPoints)
+      thetaLaser = np.average(thetaLaserPoints)
+      phiLaser = np.average(phiLaserPoints)
+      print(f"dLaser: {dLaser} {UNITS}, thetaLaser: {np.rad2deg(thetaLaser)} deg, phiLaser: {np.rad2deg(phiLaser)} deg")
    else:
-      print("ERROR: Not enough pictures were captured.")
-   '''
-
-   # Initialization
-   numImages = 8
-   distances = [
-      4.2289,
-      5.3629,
-      6.4969,
-      7.6309,
-      8.7649,
-      9.8989,
-      11.0329,
-      12.1669
-   ]
-
-   # Collect images
-   images = []
-   for i in range(numImages):
-      images.append(cv2.imread(f"Development\\Mesh Generation\\test1photos\\trial1capture{i:03d}.png"))
-   
-   # Perform all possible permutations
-   dLaserPoints = []
-   thetaLaserPoints = []
-   phiLaserPoints = []
-   for i in range(numImages):
-      for j in range(i + 1, numImages):
-         config = estimateConfiguration(images[i], distances[i], images[j], distances[j], np.deg2rad(HFOV))
-         dLaserPoints.append(config.dLaser)
-         thetaLaserPoints.append(config.thetaLaser)
-         phiLaserPoints.append(config.phiLaser)
-   
-   # Average results from all 
-   dLaser = np.average(dLaserPoints)
-   thetaLaser = np.average(thetaLaserPoints)
-   phiLaser = np.average(phiLaserPoints)
-   print(f"dLaser: {dLaser} {UNITS}, thetaLaser: {np.rad2deg(thetaLaser)} deg, phiLaser: {np.rad2deg(phiLaser)} deg")
-    
+      print("ERROR: Not enough input images captured.")
+   # end if picsRemaining == 0
 # end main
 
 '''
